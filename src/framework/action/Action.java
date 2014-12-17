10173b788f21a0503f19ad73ef8b2f3acdbc2dc0
adelmas <arnaud.delmas@ecole.ensicaen.fr>
2014-12-17 10:52:30 +0100

    Packages

---
+++ src/framework/action/Action.java
@@ -0,0 +1,34 @@
+package framework.action;
+
+import framework.board.Board;
+import framework.joueur.Player;
+
+public abstract class Action {
+	private String _action = "";
+	private int _type = 0, _x = 0, _y = 0;
+	private Player _player;
+	
+	public Action(String action, int type, int x, int y, Player player) {
+		_action = action;
+		_type = type;
+		_x = x;
+		_y = y;
+		_player = player;
+	}
+	
+	public abstract void doAction(Board board);
+	
+	public int getX() {
+		return _x;
+	}
+	public int getY() {
+		return _y;
+	}
+	public Player getPlayer() {
+		return _player;
+	}
+	
+	public String toString() {
+		return "Action() " + _action + "(" + _type + ") " + _x + ", " + _y + "\n";
+	}
+}
---
+++ src/framework/board/Case.java
@@ -0,0 +1,54 @@
+package framework.board;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Case {
+
+	/* Attributes */
+	
+	private String _typeCase;
+	private List<Piece> _pieces;
+	
+	/* Constructor */
+	
+	public Case() {
+		_pieces = new LinkedList<Piece>();
+		_typeCase = "vide";
+	}
+	
+	public Case(String type){
+		_pieces = new LinkedList<Piece>();
+		_typeCase = type;
+	}
+	
+	/* Method */
+	
+	public String getType(){
+		return _typeCase;
+	}
+	
+	public void setType(String typ){
+		_typeCase = typ;
+	}
+	
+	public List<Piece> getPieces(){
+		return _pieces;
+	}
+	
+	public Piece getFirstPiece() {
+		return _pieces.get(0);
+	}
+	
+	public void removePiece(Piece P){
+		_pieces.remove(P);
+	}
+
+	public void addPiece(Piece P){
+		_pieces.add(P);
+	}
+	
+	public boolean isEmpty(){
+		return _pieces.isEmpty();
+	}
+}
