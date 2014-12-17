9b794698ea8e958cf65c3f14e5a20ced51587541
adelmas <arnaud.delmas@ecole.ensicaen.fr>
2014-12-17 11:15:48 +0100

    package framework

--- src/framework/action/Action.java
+++ src/framework/action/Action.java
@@ -1,5 +1,4 @@
 package framework.action;
-
 import framework.board.Board;
 import framework.joueur.Player;
 
@@ -16,17 +15,57 @@ public abstract class Action {
 		_player = player;
 	}
 	
-	public abstract void doAction(Board board);
-	
-	public int getX() {
+	public int getX()
+	{
 		return _x;
 	}
-	public int getY() {
+	
+	public int getY()
+	{
 		return _y;
 	}
-	public Player getPlayer() {
+	
+	public String getAction()
+	{
+		return _action;
+	}
+	
+	public Player getPlayer() 
+	{
 		return _player;
 	}
+	
+	public void setX(int x)
+	{
+		_x = x;
+	}
+	
+	public void setY(int y)
+	{
+		_y = y;
+	}
+	
+	public void setAction(String action)
+	{
+		_action = action;
+	}
+	
+	public void setPlayer(Player player)
+	{
+		_player = player;
+	}
+	
+	public abstract void doAction(Board board);
+	
+	public void give_up()
+	{
+		_action = "GIVE_UP";
+	}
+
+	public void skip()
+	{
+		_action = "SKIP";
+	}	
 	
 	public String toString() {
 		return "Action() " + _action + "(" + _type + ") " + _x + ", " + _y + "\n";
